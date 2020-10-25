2683a8dee2b52631ca467f549026f5a0c2fad715
delphiki <delphiki@protonmail.com>
2020-10-25 22:41:35 +0100

    Replace tabs with spaces

--- extension.js
+++ extension.js
@@ -11,131 +11,133 @@ let batteryStatus;
 let statusFilePath = '/tmp/airstatus.out';
 
 class AipodsBatteryStatus {
-	constructor(menu, filePath) {
-		this._menu = menu;
-		this._statusFilePath = filePath;
-		this._box = new St.BoxLayout();
+    constructor(menu, filePath) {
+        this._menu = menu;
+        this._statusFilePath = filePath;
+        this._box = new St.BoxLayout();
 
-		this._currentStatusValue = {};
-		this._leftAirpodLabel = null;
-		this._rightAirpodLabel = null;
-		this._icon = null;
-		this._caseLabel = null;
-		this._caseIcon = null;
+        this._timeout = null;
+        this._currentStatusValue = {};
+        this._leftAirpodLabel = null;
+        this._rightAirpodLabel = null;
+        this._icon = null;
+        this._caseLabel = null;
+        this._caseIcon = null;
 
-		this.buildLayout();
-	}
+        this.buildLayout();
+    }
 
-	getCurrentStatus() {
-		if (!GLib.file_test(this._statusFilePath, GLib.FileTest.EXISTS)) {
-			return {};
-		}
+    getCurrentStatus() {
+        if (!GLib.file_test(this._statusFilePath, GLib.FileTest.EXISTS)) {
+            return {};
+        }
 
-		let fileContents = GLib.file_get_contents(this._statusFilePath)[1];
+        let fileContents = GLib.file_get_contents(this._statusFilePath)[1];
 
-		let lines;
-		if (fileContents instanceof Uint8Array) {
-			lines = ByteArray.toString(fileContents).trim().split('\n');
-		} else {
-			lines = fileContents.toString().trim().split('\n');
-		}
+        let lines;
+        if (fileContents instanceof Uint8Array) {
+            lines = ByteArray.toString(fileContents).trim().split('\n');
+        } else {
+            lines = fileContents.toString().trim().split('\n');
+        }
 
-		let lastLine = lines[lines.length - 1];
+        let lastLine = lines[lines.length - 1];
 
-		return lastLine.length > 0 ? JSON.parse(lastLine) : {};
-	}
+        return lastLine.length > 0 ? JSON.parse(lastLine) : {};
+    }
 
-	updateBatteryStatus() {
-		this._currentStatusValue = this.getCurrentStatus();
+    updateBatteryStatus() {
+        this._currentStatusValue = this.getCurrentStatus();
 
-		let charge = this._currentStatusValue.hasOwnProperty("charge") ? this._currentStatusValue.charge : {};
+        let charge = this._currentStatusValue.hasOwnProperty("charge") ? this._currentStatusValue.charge : {};
 
-		if (charge.hasOwnProperty('left') && charge.left !== -1) {
-			this._leftAirpodLabel.set_text(charge.left+' %');
-		} else {
-			this._leftAirpodLabel.set_text('- %');
-		}
-		if (charge.hasOwnProperty('right') && charge.right !== -1) {
-			this._rightAirpodLabel.set_text(charge.right+' %');
-		} else {
-			this._rightAirpodLabel.set_text('- %');
-		}
+        if (charge.hasOwnProperty('left') && charge.left !== -1) {
+            this._leftAirpodLabel.set_text(charge.left+' %');
+        } else {
+            this._leftAirpodLabel.set_text('- %');
+        }
+        if (charge.hasOwnProperty('right') && charge.right !== -1) {
+            this._rightAirpodLabel.set_text(charge.right+' %');
+        } else {
+            this._rightAirpodLabel.set_text('- %');
+        }
 
-		if (charge.hasOwnProperty('case') && charge.case !== -1) {
-			this._caseLabel.set_text(charge.case+' %');
-			this._caseLabel.show()
-			this._caseIcon.show();
-		} else {
-			this._caseLabel.hide()
-			this._caseIcon.hide();
-		}
+        if (charge.hasOwnProperty('case') && charge.case !== -1) {
+            this._caseLabel.set_text(charge.case+' %');
+            this._caseLabel.show()
+            this._caseIcon.show();
+        } else {
+            this._caseLabel.hide()
+            this._caseIcon.hide();
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	buildLayout() {
-		this._leftAirpodLabel = new St.Label({
-			text: '- %',
-			y_align: Clutter.ActorAlign.CENTER,
-			style_class: "left-airpod-label"
-		});
+    buildLayout() {
+        this._leftAirpodLabel = new St.Label({
+            text: '- %',
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: "left-airpod-label"
+        });
 
-		this._icon = new St.Icon({
-			gicon: Gio.icon_new_for_string(Me.path + '/airpods.svg'),
-			style_class: "system-status-icon",
-		});
+        this._icon = new St.Icon({
+            gicon: Gio.icon_new_for_string(Me.path + '/airpods.svg'),
+            style_class: "system-status-icon",
+        });
 
-		this._rightAirpodLabel = new St.Label({
-			text: '- %',
-			y_align: Clutter.ActorAlign.CENTER,
-			style_class: "right-airpod-label"
-		});
+        this._rightAirpodLabel = new St.Label({
+            text: '- %',
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: "right-airpod-label"
+        });
 
-		this._caseIcon = new St.Icon({
-			gicon: Gio.icon_new_for_string(Me.path + '/case.svg'),
-			style_class: "system-status-icon",
-		});
+        this._caseIcon = new St.Icon({
+            gicon: Gio.icon_new_for_string(Me.path + '/case.svg'),
+            style_class: "system-status-icon",
+        });
 
-		this._caseLabel = new St.Label({
-			text: '- %',
-			y_align: Clutter.ActorAlign.CENTER,
-			style_class: "right-airpod-label"
-		});
+        this._caseLabel = new St.Label({
+            text: '- %',
+            y_align: Clutter.ActorAlign.CENTER,
+            style_class: "right-airpod-label"
+        });
 
-		this._box.add(this._leftAirpodLabel)
-		this._box.add(this._icon);
-		this._box.add(this._rightAirpodLabel);
-		this._box.add(this._caseIcon);
-		this._box.add(this._caseLabel);
-	}
+        this._box.add(this._leftAirpodLabel)
+        this._box.add(this._icon);
+        this._box.add(this._rightAirpodLabel);
+        this._box.add(this._caseIcon);
+        this._box.add(this._caseLabel);
+    }
 
-	enable() {
-		this._menu.insert_child_at_index(this._box, 0);
+    enable() {
+        this._menu.insert_child_at_index(this._box, 0);
 
-		let self = this;
-		Mainloop.timeout_add(10000, function() {
-			return self.updateBatteryStatus();
-		});
-	}
+        let self = this;
+        this._timeout = Mainloop.timeout_add(10000, function() {
+            return self.updateBatteryStatus();
+        });
+    }
 
-	disable() {
-		this._menu.remove_child(this._box);
-	}
+    disable() {
+        this._menu.remove_child(this._box);
+        Mainloop.source_remove(this._timeout);
+    }
 }
 
 function init() {
-	let menu = Main.panel.statusArea["aggregateMenu"]._power;
-	batteryStatus = new AipodsBatteryStatus(menu, statusFilePath);
+    let menu = Main.panel.statusArea["aggregateMenu"]._power;
+    batteryStatus = new AipodsBatteryStatus(menu, statusFilePath);
 }
 
 function enable() {
-	batteryStatus.enable();
+    batteryStatus.enable();
 }
 
 function disable() {
-	batteryStatus.disable();
+    batteryStatus.disable();
 }
 
 let Log = function(msg) {
-	log("[Airpods Battery Status] " + msg);
+    log("[Airpods Battery Status] " + msg);
 }
