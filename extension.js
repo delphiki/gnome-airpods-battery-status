2051b3ba1caa3d8e0b8017e4cfccfe14b264ac21
delphiki <delphiki@protonmail.com>
2020-11-07 23:41:22 +0100

    Add cache for battery levels

--- extension.js
+++ extension.js
@@ -9,6 +9,7 @@ const Me = imports.misc.extensionUtils.getCurrentExtension();
 
 let batteryStatus;
 let statusFilePath = '/tmp/airstatus.out';
+let cacheTTL = 3600;
 
 class AipodsBatteryStatus {
     constructor(menu, filePath) {
@@ -18,6 +19,11 @@ class AipodsBatteryStatus {
 
         this._timeout = null;
         this._currentStatusValue = {};
+        this._cache = {
+            leftUpdatedAt: null,
+            rightUpdatedAt: null,
+            caseUpdatedAt: null,
+        };
         this._leftAirpodLabel = null;
         this._rightAirpodLabel = null;
         this._icon = null;
@@ -50,23 +56,42 @@ class AipodsBatteryStatus {
         this._currentStatusValue = this.getCurrentStatus();
 
         let charge = this._currentStatusValue.hasOwnProperty("charge") ? this._currentStatusValue.charge : {};
+        let statusDate = this._currentStatusValue.hasOwnProperty('date') ? Date.parse(this._currentStatusValue.date) : null;
+        let now = Date.now();
+        let cacheLimitDate = now - (cacheTTL * 1000);
+        let statusTooOld = statusDate < cacheLimitDate;
 
-        if (charge.hasOwnProperty('left') && charge.left !== -1) {
+        if (!statusTooOld && charge.hasOwnProperty('left') && charge.left !== -1) {
             this._leftAirpodLabel.set_text(charge.left+' %');
-        } else {
+            this._cache.leftUpdatedAt = statusDate;
+        } else if (
+            this._cache.leftUpdatedAt === null
+            || this._cache.leftUpdatedAt < cacheLimitDate
+        ) {
             this._leftAirpodLabel.set_text('- %');
         }
-        if (charge.hasOwnProperty('right') && charge.right !== -1) {
+
+        if (!statusTooOld && charge.hasOwnProperty('right') && charge.right !== -1) {
             this._rightAirpodLabel.set_text(charge.right+' %');
-        } else {
+            this._cache.rightUpdatedAt = statusDate;
+        } else if (
+            statusTooOld
+            || this._cache.rightUpdatedAt === null
+            || this._cache.rightUpdatedAt < cacheLimitDate
+        ) {
             this._rightAirpodLabel.set_text('- %');
         }
 
-        if (charge.hasOwnProperty('case') && charge.case !== -1) {
+        if (!statusTooOld && charge.hasOwnProperty('case') && charge.case !== -1) {
             this._caseLabel.set_text(charge.case+' %');
+            this._cache.caseUpdatedAt = statusDate;
             this._caseLabel.show()
             this._caseIcon.show();
-        } else {
+        } else if (
+            statusTooOld
+            || this._cache.caseUpdatedAt === null
+            || this._cache.caseUpdatedAt < cacheLimitDate
+        ) {
             this._caseLabel.hide()
             this._caseIcon.hide();
         }
