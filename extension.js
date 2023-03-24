519d382f7bbd3b55447ed5d1dcbda890feec093c
delphiki <delphiki@protonmail.com>
2023-03-24 17:14:10 +0100

    feat: add Gnome Shell 43 compatibility + popup menu

--- extension.js
+++ extension.js
@@ -5,24 +5,30 @@ const Main = imports.ui.main;
 const { Clutter, GLib, St, Gio } = imports.gi;
 const ByteArray = imports.byteArray;
 const Mainloop = imports.mainloop;
+const PanelMenu = imports.ui.panelMenu;
 const Me = imports.misc.extensionUtils.getCurrentExtension();
+const PopupMenu = imports.ui.popupMenu;
+
 
 let batteryStatus;
 let statusFilePath = '/tmp/airstatus.out';
 let cacheTTL = 3600;
 
 class AipodsBatteryStatus {
-    constructor(menu, filePath) {
-        this._menu = menu;
+    constructor(filePath) {
         this._statusFilePath = filePath;
-        this._box = new St.BoxLayout();
+        this._panelMenuButton = null;
 
-        this._timeout = null;
+        this._timer = null;
         this._currentStatusValue = {};
         this._cache = {
             leftUpdatedAt: null,
             rightUpdatedAt: null,
             caseUpdatedAt: null,
+            modelUpdatedAt: null,
+            leftChargingUpdatedAt: null,
+            rightChargingUpdatedAt: null,
+            caseChargingUpdatedAt: null,
         };
         this._leftAirpodLabel = null;
         this._rightAirpodLabel = null;
@@ -30,10 +36,22 @@ class AipodsBatteryStatus {
         this._caseLabel = null;
         this._caseIcon = null;
 
+
+        this._subMenuModelItem = null;
+        this._subMenuModelItemLabel = null;
+
+        this._subMenuLeftChargingItem = null;
+        this._subMenuRightChargingItem = null;
+        this._subMenuCaseChargingItem = null;
+
         this.buildLayout();
+
+        this.updateBatteryStatus();
     }
 
     getCurrentStatus() {
+        Log("getCurrentStatus");
+
         if (!GLib.file_test(this._statusFilePath, GLib.FileTest.EXISTS)) {
             return {};
         }
@@ -53,6 +71,8 @@ class AipodsBatteryStatus {
     }
 
     updateBatteryStatus() {
+        Log("updateBatteryStatus");
+
         this._currentStatusValue = this.getCurrentStatus();
 
         let charge = this._currentStatusValue.hasOwnProperty("charge") ? this._currentStatusValue.charge : {};
@@ -61,26 +81,17 @@ class AipodsBatteryStatus {
         let cacheLimitDate = now - (cacheTTL * 1000);
         let statusTooOld = statusDate < cacheLimitDate;
 
-        if (!statusTooOld && charge.hasOwnProperty('left') && charge.left !== -1) {
-            this._leftAirpodLabel.set_text(charge.left+' %');
-            this._cache.leftUpdatedAt = statusDate;
-        } else if (
-            this._cache.leftUpdatedAt === null
-            || this._cache.leftUpdatedAt < cacheLimitDate
-        ) {
-            this._leftAirpodLabel.set_text('- %');
-        }
-
-        if (!statusTooOld && charge.hasOwnProperty('right') && charge.right !== -1) {
-            this._rightAirpodLabel.set_text(charge.right+' %');
-            this._cache.rightUpdatedAt = statusDate;
-        } else if (
-            statusTooOld
-            || this._cache.rightUpdatedAt === null
-            || this._cache.rightUpdatedAt < cacheLimitDate
-        ) {
-            this._rightAirpodLabel.set_text('- %');
-        }
+        ['left', 'right'].forEach((chargeable) => {
+            if (!statusTooOld && charge.hasOwnProperty(chargeable) && charge[chargeable] !== -1) {
+                this['_'+chargeable+'AirpodLabel'].set_text(charge[chargeable]+' %');
+                this._cache[chargeable+'UpdatedAt'] = statusDate;
+            } else if (
+                this._cache[chargeable+'UpdatedAt'] === null
+                || this._cache[chargeable+'UpdatedAt'] < cacheLimitDate
+            ) {
+                this['_'+chargeable+'AirpodLabel'].set_text('...');
+            }
+        });
 
         if (!statusTooOld && charge.hasOwnProperty('case') && charge.case !== -1) {
             this._caseLabel.set_text(charge.case+' %');
@@ -96,12 +107,41 @@ class AipodsBatteryStatus {
             this._caseIcon.hide();
         }
 
+        if (!statusTooOld && this._currentStatusValue.hasOwnProperty('model')) {
+            this._subMenuModelItemLabel.set_text(this._currentStatusValue.model.split(/(?=[A-Z0-9])/).join(' '));
+        } else if (
+                statusTooOld
+                || this._cache.modelUpdatedAt === null
+                || this._cache.modelUpdatedAt < cacheLimitDate
+                ) {
+            this._subMenuModelItemLabel.set_text('no Airpods detected');
+        }
+
+        ['case', 'left', 'right'].forEach((chargeable) => {
+            if (!statusTooOld && this._currentStatusValue.hasOwnProperty('charging_'+chargeable)) {
+                this['_subMenu'+this.capitalize(chargeable)+'ChargingItem'].label.text = this.capitalize(chargeable)+': '
+                    +(this._currentStatusValue['charging_'+chargeable] ? 'charging' : 'not charging')
+                    +(charge[chargeable] !== -1 ? ' ('+charge[chargeable]+' %)' : '')
+                ;
+                this['_subMenu'+this.capitalize(chargeable)+'ChargingItem'].setIcon(this.getBatteryIcon(
+                    charge[chargeable],
+                    this._currentStatusValue['charging_'+chargeable]
+                ));
+            } else {
+                this['_subMenu'+this.capitalize(chargeable)+'ChargingItem'].label.text = this.capitalize(chargeable)+': N/A';
+            }
+        });
+
         return true;
     }
 
     buildLayout() {
+        Log("buildLayout");
+
+        let box = new St.BoxLayout();
+
         this._leftAirpodLabel = new St.Label({
-            text: '- %',
+            text: '...',
             y_align: Clutter.ActorAlign.CENTER,
             style_class: "left-airpod-label"
         });
@@ -112,7 +152,7 @@ class AipodsBatteryStatus {
         });
 
         this._rightAirpodLabel = new St.Label({
-            text: '- %',
+            text: '...',
             y_align: Clutter.ActorAlign.CENTER,
             style_class: "right-airpod-label"
         });
@@ -123,36 +163,92 @@ class AipodsBatteryStatus {
         });
 
         this._caseLabel = new St.Label({
-            text: '- %',
+            text: '...',
             y_align: Clutter.ActorAlign.CENTER,
             style_class: "right-airpod-label"
         });
 
-        this._box.add(this._leftAirpodLabel)
-        this._box.add(this._icon);
-        this._box.add(this._rightAirpodLabel);
-        this._box.add(this._caseIcon);
-        this._box.add(this._caseLabel);
+        box.add(this._leftAirpodLabel)
+        box.add(this._icon);
+        box.add(this._rightAirpodLabel);
+        box.add(this._caseIcon);
+        box.add(this._caseLabel);
+
+        this._panelMenuButton = new PanelMenu.Button(0.5, 'AirpodsBatteryStatusPopup', false);
+
+        this._panelMenuButton.add_child(box);
+
+        this._subMenuModelItem = new PopupMenu.PopupMenuItem('Model: ');
+        this._subMenuModelItemLabel = new St.Label({ text : 'no Airpods detected' });
+        this._subMenuModelItem.add_child(this._subMenuModelItemLabel);
+
+        this._subMenuCaseChargingItem = new PopupMenu.PopupImageMenuItem('Case: not charging', 'battery-missing');
+        this._subMenuLeftChargingItem = new PopupMenu.PopupImageMenuItem('Left pod: not charging', 'battery-missing');
+        this._subMenuRightChargingItem = new PopupMenu.PopupImageMenuItem('Right pod: not charging', 'battery-missing');
+
+        this._panelMenuButton.menu.addMenuItem(this._subMenuModelItem);
+        this._panelMenuButton.menu.addMenuItem(this._subMenuCaseChargingItem);
+        this._panelMenuButton.menu.addMenuItem(this._subMenuLeftChargingItem);
+        this._panelMenuButton.menu.addMenuItem(this._subMenuRightChargingItem);
+
+        Main.panel.addToStatusArea("AirpodsBatteryStatus", this._panelMenuButton, 1);
+    }
+
+    getBatteryIcon(percentage, charging) {
+        let iconName = 'battery-';
+        switch (true) {
+            case percentage < 0:
+                return 'battery-missing';
+            case percentage <= 10:
+                iconName += 'caution';
+                break;
+            case percentage <= 40:
+                iconName += 'low';
+                break;
+            case percentage <= 70:
+                iconName += 'good';
+                break;
+            case percentage <= 100:
+                iconName += 'full';
+                break;
+            default:
+                return 'battery-missing';
+        }
+
+        return iconName + (charging ? '-charging' : '');
+    }
+
+    capitalize(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
     enable() {
-        this._menu.insert_child_at_index(this._box, 0);
+        Log("enable");
+        this.updateBatteryStatus();
 
-        let self = this;
-        this._timeout = Mainloop.timeout_add(10000, function() {
-            return self.updateBatteryStatus();
+        this._timer = Mainloop.timeout_add_seconds(10, () => {
+            this.updateBatteryStatus();
+            return GLib.SOURCE_CONTINUE;
         });
     }
 
     disable() {
-        this._menu.remove_child(this._box);
-        Mainloop.source_remove(this._timeout);
+        Log("disable");
+
+        this._panelMenuButton.destroy();
+        Main.panel.statusArea["AirpodsBatteryStatus"] = null;
+
+        if (this._timer) {
+            Mainloop.source_remove(this._timer);
+            this._timer = null;
+        }
     }
 }
 
 function enable() {
-    let menu = Main.panel.statusArea["aggregateMenu"]._power;
-    batteryStatus = new AipodsBatteryStatus(menu, statusFilePath);
+    batteryStatus = new AipodsBatteryStatus(statusFilePath);
+    Main.panel.statusArea["AirpodsBatteryStatus"] = null;
+
     batteryStatus.enable();
 }
 
